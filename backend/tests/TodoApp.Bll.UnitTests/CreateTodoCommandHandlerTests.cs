59e4a0326b50730cdae261987aa19a695b0bcbc5
Bede Fülöp <bede.fulop@encosoft.hu>
2021-07-17 17:20:14 +0200

    unit tests for Create and Patch command handlers

---
+++ backend/tests/TodoApp.Bll.UnitTests/CreateTodoCommandHandlerTests.cs
@@ -0,0 +1,39 @@
+using NSubstitute;
+using System.Threading.Tasks;
+using TodoApp.Bll.Features;
+using TodoApp.Domain.Model;
+using TodoApp.Domain.Repositories;
+using Xunit;
+
+namespace TodoApp.Bll.UnitTests
+{
+    public class CreateTodoCommandHandlerTests
+    {
+        private readonly CreateTodoCommandHandler sut;
+        private readonly ITodoRepository todoRepository = Substitute.For<ITodoRepository>();
+
+        public CreateTodoCommandHandlerTests()
+        {
+            sut = new CreateTodoCommandHandler(todoRepository);
+        }
+
+        [Fact]
+        public async Task CreateTodoCommandHandler_ShouldCreateTodo_WhenAllParametersAreValid()
+        {
+            // Arrange
+            var createTodoCommand = new CreateTodoCommand
+            {
+                Description = "Test todo"
+            };
+
+            // Act
+            await sut.Handle(createTodoCommand, default);
+
+            // Assert
+            todoRepository.Received().Insert(Arg.Is<Todo>(todo => 
+                todo.Description == createTodoCommand.Description
+                && todo.IsComplete == false
+                && todo.Id == 0));
+        }
+    }
+}
---
+++ backend/tests/TodoApp.Bll.UnitTests/PatchTodoCommandHandlerTests.cs
@@ -0,0 +1,80 @@
+﻿using NSubstitute;
+using System.Threading.Tasks;
+using TodoApp.Bll.Features;
+using TodoApp.Domain.Model;
+using TodoApp.Domain.Repositories;
+using Xunit;
+
+namespace TodoApp.Bll.UnitTests
+{
+    public class PatchTodoCommandHandlerTests
+    {
+        private readonly PatchTodoCommandHandler sut;
+        private readonly ITodoRepository todoRepository = Substitute.For<ITodoRepository>();
+
+        public PatchTodoCommandHandlerTests()
+        {
+            sut = new PatchTodoCommandHandler(todoRepository);
+        }
+
+        [Fact]
+        public async Task PatchTodoCommandHandler_ShouldPatchDescriptionOnly_WhenDescriptionIsNotNullAndIsCompleteIsNull()
+        {
+            // Arrange
+            var todoId = 1;
+
+            var patchTodoCommand = new PatchTodoCommand
+            {
+                Id = todoId,
+                Description = "Modified description",
+                IsComplete = null
+            };
+
+            var todoEntity = new Todo("Original description")
+            {
+                Id = todoId
+            };
+
+            todoRepository.GetAsync(patchTodoCommand.Id).Returns(todoEntity);
+
+            // Act
+            await sut.Handle(patchTodoCommand, default);
+
+            // Assert
+            todoRepository.Received().Update(Arg.Is<Todo>(todo =>
+                todo.Id == patchTodoCommand.Id
+                && todo.Description == patchTodoCommand.Description
+                && todo.IsComplete == todoEntity.IsComplete));
+        }
+
+        [Fact]
+        public async Task PatchTodoCommandHandler_ShouldPatchIsCompleteOnly_WhenIsCompleteIsNotNullAndDescriptionIsNull()
+        {
+            // Arrange
+            var todoId = 1;
+
+            var patchTodoCommand = new PatchTodoCommand
+            {
+                Id = todoId,
+                Description = null,
+                IsComplete = true
+            };
+
+            var todoEntity = new Todo("Original description")
+            {
+                Id = todoId
+            };
+
+            todoRepository.GetAsync(patchTodoCommand.Id).Returns(todoEntity);
+
+            // Act
+            await sut.Handle(patchTodoCommand, default);
+
+            // Assert
+            todoRepository.Received().Update(Arg.Is<Todo>(todo =>
+                todo.Id == patchTodoCommand.Id
+                && todo.IsComplete == patchTodoCommand.IsComplete
+                && todo.Description == todoEntity.Description));
+        }
+    }
+}
