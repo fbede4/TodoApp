3c68bf48da831eb0d00996ee0bdbe7ff47629f00
Bede Fülöp <bede.fulop@encosoft.hu>
2021-07-17 08:15:32 +0200

    remove IIS profile and add domain model

---
+++ backend/TodoApp.Domain/Model/Todo.cs
@@ -0,0 +1,16 @@
+﻿namespace TodoApp.Domain.Model
+{
+    public class Todo
+    {
+        public Todo(string description)
+        {
+            Description = description;
+        }
+
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
