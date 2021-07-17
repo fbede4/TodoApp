e45933de039dade3365582e0a166f9f5a0345da0
Bede Fülöp <bede.fulop@encosoft.hu>
2021-07-17 17:19:38 +0200

    TodoApi integration tests

---
+++ backend/tests/TodoApp.Api.IntegrationTests/Scenarios/DeleteTodoTests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TodoApp.Dal;
+using TodoApp.Domain.Model;
+using Xunit;
+
+namespace TodoApp.Api.IntegrationTests.Scenarios
+{
+    public class DeleteTodoTests : IClassFixture<TestWebApplicationFactory>
+    {
+        protected readonly WebApplicationFactory<Startup> factory;
+        protected readonly HttpClient httpClient;
+
+        public DeleteTodoTests()
+        {
+            factory = new TestWebApplicationFactory();
+            httpClient = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task DeleteTodoEndpoint_ShouldDeleteTodo_WhenIdExists()
+        {
+            // Arrange
+            var todo = new Todo("Some description");
+
+            using (var scope = factory.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<TodoAppDbContext>();
+                dbContext.Todos.Add(todo);
+                await dbContext.SaveChangesAsync();
+            }
+
+            // Act
+            var httpResponseMessage = await httpClient.DeleteAsync($"api/todos/{todo.Id}");
+
+            // Assert
+            httpResponseMessage.StatusCode.Should().Be(200);
+
+            using (var scope = factory.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<TodoAppDbContext>();
+
+                var todoExists = await dbContext.Todos.AnyAsync(t => t.Id == todo.Id);
+                todoExists.Should().Be(false);
+            }
+        }
+
+        [Fact]
+        public async Task DeleteTodoEndpoint_ShouldReturnNotFound_WhenIdDoesNotExist()
+        {
+            // Arange
+            var nonExistentId = 14;
+
+            // Act
+            var httpResponseMessage = await httpClient.DeleteAsync($"todos/{nonExistentId}");
+
+            // Assert
+            httpResponseMessage.StatusCode.Should().Be(404);
+        }
+    }
+}
---
+++ backend/tests/TodoApp.Api.IntegrationTests/TestWebApplicationFactory.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using TodoApp.Dal;
+
+namespace TodoApp.Api.IntegrationTests
+{
+    public class TestWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder
+                .ConfigureServices(services =>
+                {
+                    services.AddControllers()
+                        .AddApplicationPart(Assembly.Load("TodoApp.Api"))
+                        .AddFluentValidation(opt =>
+                        {
+                            opt.RegisterValidatorsFromAssembly(Assembly.Load("TodoApp.Bll"));
+                        });
+
+                    services.AddDbContext<TodoAppDbContext>(opt =>
+                    {
+                        opt.UseInMemoryDatabase("InMemoryDbForTesting");
+                    });
+
+                    var serviceProvider = services.BuildServiceProvider();
+
+                    using var scope = serviceProvider.CreateScope();
+                    var dbContext = scope.ServiceProvider.GetRequiredService<TodoAppDbContext>();
+
+                    dbContext.Database.EnsureCreated();
+                });
+        }
+    }
+}
