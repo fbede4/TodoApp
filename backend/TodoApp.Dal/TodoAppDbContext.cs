a6c1630a2853bf44f5b3a883dce9cd59251098c3
Bede Fülöp <bede.fulop@encosoft.hu>
2021-07-17 09:01:14 +0200

    create DbContext and add automigrate functionality

---
+++ backend/TodoApp.Dal/TodoAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Domain.Model;
+
+namespace TodoApp.Dal
+{
+    public class TodoAppDbContext : DbContext
+    {
+        public TodoAppDbContext(DbContextOptions<TodoAppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
